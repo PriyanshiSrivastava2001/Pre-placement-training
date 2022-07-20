0135e32b5c9a741d4149ee891f08a2a2ad199465
PriyanshiSrivastava2001 <66244412+PriyanshiSrivastava2001@users.noreply.github.com>
2022-07-20 18:26:57 +0530

    Create binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,30 @@
+def binarySearch(array, x, low, high):
+
+    if high >= low:
+
+        mid = low + (high - low)//2
+        if array[mid] == x:
+            return mid
+        elif array[mid] > x:
+            return binarySearch(array, x, low, mid-1)
+        else:
+            return binarySearch(array, x, mid + 1, high)
+
+    else:
+        return -1
+
+
+array = [4, 5, 7, 8, 9]
+x = 7
+
+result = binarySearch(array, x, 0, len(array)-1)
+
+if result != -1:
+    print("Element is present at index " + str(result))
+else:
+    print("Not found")
+    
+    
+'''OUTPUT-
+Element is present at index 2
+'''
